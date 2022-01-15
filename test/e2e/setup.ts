38431f40f94e26f8e9ebd15808c20bc98a9e4f9d
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2022-01-15 01:00:48 -0300

    Fix Action CI GitHub

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -3,14 +3,6 @@ import knexfile from '../../knexfile';
 
 const database = knex(knexfile.test);
 
-beforeAll(() => {
-  database('exams').truncate();
-  database('associations').truncate();
-  database('laboratories').truncate();
-});
-
-afterAll(() => {
-  database('exams').truncate();
-  database('associations').truncate();
-  database('laboratories').truncate();
+afterAll(async () => {
+  await database('exams').truncate();
 });
