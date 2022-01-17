ec7d8b1862f2cfdd638b02a514ad60fbc4af9811
Murilo Eduardo dos Santos <murilo.santos@somosphi.com>
2022-01-17 10:25:18 -0300

    Try fix database

--- src/util/database.ts
+++ src/util/database.ts
@@ -3,12 +3,19 @@ import knex, { Knex } from 'knex';
 import knexfile from '../../knexfile';
 
 export default class Database {
-  private config =
-    env.nodeEnv === 'test' ? knexfile.test : knexfile.development;
+  private config;
   private _db!: Knex;
   private _tbName!: string;
 
   constructor() {
+    if (env.nodeEnv === 'test') {
+      this.config = knexfile.test;
+    } else if (env.nodeEnv === 'production') {
+      this.config = knexfile.production;
+    } else {
+      this.config = knexfile.production;
+    }
+
     this._db = this._db || knex(this.config);
   }
 
